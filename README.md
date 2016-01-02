fe925b926755a7e2d8a67f1f6c8165f0e2708253
Jeremy Wilken <gnomation@gnomeontherun.com>
2016-01-02 11:25:25 -0600

    pipes example

---
+++ README.md
@@ -0,0 +1,5 @@
+Learning about Pipes in Angular2.
+
+### Setup
+
+Just run `npm install` to get everything setup. Then run `npm start` to start running the app.
---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    Pipes Chapter
+  `
+})
+export class App {
+
+}
